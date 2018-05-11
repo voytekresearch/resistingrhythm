1f50d94e168f3fcab14382e54bbd988b61cb4110
Erik Peterson <Erik.Exists@gmail.com>
2018-05-11 10:59:32 -0700

    ....

--- resistingrhythm/ipynb/testing_HHH.py
+++ resistingrhythm/ipynb/testing_HHH.py
@@ -16,7 +16,7 @@ from resistingrhythm.neurons import HHH
 
 #%% -------------------------------------------------------------------
 # Shared Params
-time = 1
+time = 4
 N = 1
 
 V_e = 0
@@ -25,13 +25,16 @@ V_i = -80e-3
 tau_e = 5e-3
 tau_i = 10e-3
 
-tau_h = 4
+tau_h = 2
+Ca_target = 0.024
 
-w = (5e-6, 50e-6)
+w = (5e-6, 25e-6)
+
+noise_rate = 6  # 20
 
 #%% -------------------------------------------------------------------
 # Load data
-osc_name = "/Users/type/Code/resistingrhythm/data/osc160.csv"
+osc_name = "/Users/type/Code/resistingrhythm/data/osc1180.csv"
 stim_name = "/Users/type/Code/resistingrhythm/data/stim0.csv"
 
 ns_osc, ts_osc = load_spikes(osc_name)
@@ -55,31 +58,34 @@ show(p)
 # Run HHH
 results = HHH(
     time,
-    # ns_in,
-    # ts_in,
-    # ns_osc,
-    # ts_osc,
-    np.asarray([]),  # stim
-    np.asarray([]),
-    np.asarray([]),  # osc
-    np.asarray([]),
+    ns_in,
+    ts_in,
+    ns_osc,
+    ts_osc,
+    # np.asarray([]),  # stim
+    # np.asarray([]),
+    # np.asarray([]),  # osc
+    # np.asarray([]),
     external_current=None,
     N=N,
-    Ca_target=0.03,
+    Ca_target=Ca_target,
     tau_h=tau_h,
     w_in=w,
     tau_in=tau_e,
     V_in=V_e,
     bias_in=0.0e-9,
     w_osc=w,
-    tau_osc=tau_e,
-    V_osc=V_e,
-    noise_rate=1,
-    homeostasis=False,
+    tau_osc=tau_i,
+    V_osc=V_i,
+    noise_rate=noise_rate,
+    homeostasis=True,
     time_step=1e-5,
-    seed_value=42)
+    seed_value=12)
 
 #%%
+print(">>> HHH firing rate {}".format(results['ns'].size / N / time))
+
+#%% -------------------------------------------------------------------
 # Plot HHH
 p = figure(plot_width=600, plot_height=300)
 p.circle(results['ts'], results['ns'], color="black")
@@ -120,11 +126,12 @@ p = figure(plot_width=600, plot_height=300)
 for n in range(N):
     v = results['calcium'][n, :]
     p.line(x=results['times'], y=v, color="black", alpha=0.5)
+    p.line(x=results['times'], y=Ca_target, color="purple", alpha=0.2)
 p.xaxis.axis_label = 'Time (s)'
 p.yaxis.axis_label = 'Ca (moles)'
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
-p.x_range = Range1d(0, time)
+p.x_range = Range1d(0, 2)
 show(p)
 
 #%% -------------------------------------------------------------------
@@ -148,15 +155,15 @@ for n in range(N):
         color="red",
         alpha=1,
         legend="Na")
-    p.line(
-        x=results['times'],
-        y=results['g_Ca'][n, :],
-        color="purple",
-        alpha=1,
-        legend="Ca")
+    # p.line(
+    #     x=results['times'],
+    #     y=results['g_Ca'][n, :],
+    #     color="purple",
+    #     alpha=1,
+    #     legend="Ca")
 p.xaxis.axis_label = 'Time (s)'
 p.yaxis.axis_label = 'g (siemens)'
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = None
-p.x_range = Range1d(0, time)
+p.x_range = Range1d(0, 1)
 show(p)
